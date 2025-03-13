52d0bcc73f8e1e2ea310057d4b997fe16553000e
Carens <carens.gabriel@gmail.com>
2025-03-13 15:58:14 +0700

    modified database.py

--- database.py
+++ database.py
@@ -2,5 +2,5 @@ from flask_pymongo import PyMongo
 from flask import Flask
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/adopsi"
+app.config["MONGO_URI"] = "mongodb+srv://carens:9GSwmL3RTPekTJQR@adoptme.vngse.mongodb.net/"
 mongo = PyMongo(app)
