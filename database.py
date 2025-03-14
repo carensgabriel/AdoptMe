0a1dc7f6fd95c483cf1d166442b5ee0c2041a719
Carens <carens.gabriel@gmail.com>
2025-03-14 09:16:33 +0700

    update database

--- database.py
+++ database.py
@@ -2,5 +2,6 @@ from flask_pymongo import PyMongo
 from flask import Flask
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb+srv://carens:9GSwmL3RTPekTJQR@adoptme.vngse.mongodb.net/"
+app.config["MONGO_URI"] = "mongodb+srv://carens:9GSwmL3RTPekTJQR@adoptme.vngse.mongodb.net/AdoptMe"
+# app.config["MONGO_URI"] = "mongodb+srv://<username>:<password>@<cluster-name>.mongodb.net/<database>?retryWrites=true&w=majority"
 mongo = PyMongo(app)
