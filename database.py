6b150cea62bdaeb8395d37919a16818e1d206b3e
Carens <carens.gabriel@gmail.com>
2025-03-14 10:00:54 +0700

    undo database & run

--- database.py
+++ database.py
@@ -1,11 +1,11 @@
-# from flask_pymongo import PyMongo
+from flask_pymongo import PyMongo
 
-# mongo = PyMongo()
+mongo = PyMongo()
 
 # from flask_pymongo import PyMongo
 # from flask import Flask
 
 # app = Flask(__name__)
-# app.config["MONGO_URI"] = "mongodb+srv://carens:9GSwmL3RTPekTJQR@AdoptMe.vngse.mongodb.net/adopsi"
+# mongo.config["MONGO_URI"] = "mongodb+srv://carens:9GSwmL3RTPekTJQR@AdoptMe.vngse.mongodb.net/adopsi"
 # # app.config["MONGO_URI"] = "mongodb+srv://<username>:<password>@<cluster-name>.mongodb.net/<database>?retryWrites=true&w=majority"
 # mongo = PyMongo(app)
