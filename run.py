337f8639b62da3f48a99a46a984df6039bbe74a8
Carens <carens.gabriel@gmail.com>
2025-03-17 13:51:56 +0700

    update run

--- run.py
+++ run.py
@@ -4,11 +4,18 @@ from routes.user_routes import user_bp
 from routes.admin_routes import admin_bp
 from routes.animal_routes import animal_bp
 from database import mongo
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "supersecretkey"
-app.config["MONGO_URI"] = "mongodb+srv://carens:uMR6GKMHifGFACmL@AdoptMe.vngse.mongodb.net/adopsi"
 
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "fallback_secret_key")
+MONGO_URI = os.getenv("MONGO_URI")
+if not MONGO_URI:
+    raise ValueError("MONGO_URI tidak ditemukan! Pastikan variabel lingkungan sudah diatur.")
+
+app.config["MONGO_URI"] = MONGO_URI
 mongo.init_app(app)
 
 # Register Blueprints
@@ -22,4 +29,5 @@ def index():
     return render_template("home.html")
 
 if __name__ == "__main__":
-    app.run(port=3333, debug=True)
+    debug_mode = os.getenv("FLASK_DEBUG", "False").lower() == "True"
+    app.run(port=3333, debug=debug_mode)
