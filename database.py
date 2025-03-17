5d232e51bdd5f28e84c67664df4e66ec624a37b0
Carens <carens.gabriel@gmail.com>
2025-03-17 11:00:44 +0700

    update database.py

--- database.py
+++ database.py
@@ -1,11 +1,3 @@
 from flask_pymongo import PyMongo
 
 mongo = PyMongo()
-
-# from flask_pymongo import PyMongo
-# from flask import Flask
-
-# app = Flask(__name__)
-# mongo.config["MONGO_URI"] = "mongodb+srv://carens:9GSwmL3RTPekTJQR@AdoptMe.vngse.mongodb.net/adopsi"
-# # app.config["MONGO_URI"] = "mongodb+srv://<username>:<password>@<cluster-name>.mongodb.net/<database>?retryWrites=true&w=majority"
-# mongo = PyMongo(app)
